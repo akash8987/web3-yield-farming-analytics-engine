c20a62c7e4954f9974d12b5b82c1707965b076eb
Akash <161524961+akash8987@users.noreply.github.com>
2026-01-18 15:03:16 +0600

    Create farm.registry.js

---
+++ farm.registry.js
@@ -0,0 +1,11 @@
+export function loadFarms() {
+  console.log("Loading farming pools...");
+
+  return [
+    {
+      name: "ETH-USDC Farm",
+      rewardRate: 0.002,
+      totalLiquidity: 500000
+    }
+  ];
+}
